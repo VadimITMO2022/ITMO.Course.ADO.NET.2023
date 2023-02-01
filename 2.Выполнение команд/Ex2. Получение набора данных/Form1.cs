2710f66b1535c570a746bcd134dbf05427329e4b
VadimITMO2022 <116649038+VadimITMO2022@users.noreply.github.com>
2023-02-01 18:33:32 +0300

    Add files via upload

---
+++ 2.Выполнение команд/Ex2. Получение набора данных/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO1_WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string connectionString = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;User ID=LAPTOP-6G0GA05G\vadim;Initial Catalog=Northwind;Data Source=LAPTOP-6G0GA05G\SQLEXPRESS";
+
+
+            // создание подключения
+            OleDbConnection conn = new OleDbConnection(connectionString);
+
+            // открываем подключение
+            conn.Open();
+
+            OleDbCommand command = conn.CreateCommand();
+            command.CommandText = "SELECT ProductName FROM Products";
+            OleDbDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                listView1.Items.Add(reader["ProductName"].ToString());
+            }
+
+        }
+  
+    }
+}
