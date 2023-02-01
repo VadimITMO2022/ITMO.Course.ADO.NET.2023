6debd7ac7e82c45c6b5674098bed70e3eb1c0caf
VadimITMO2022 <116649038+VadimITMO2022@users.noreply.github.com>
2023-02-01 18:32:45 +0300

    Add files via upload

---
+++ 2.Выполнение команд/Ex1. Получение скалярного значения/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection.Emit;
+
+namespace ADO1_WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string connectionString = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;User ID=LAPTOP-6G0GA05G\vadim;Initial Catalog=Northwind;Data Source=LAPTOP-6G0GA05G\SQLEXPRESS";
+
+
+            // создание подключения
+            OleDbConnection conn = new OleDbConnection(connectionString);
+
+            // открываем подключение
+            conn.Open();
+
+            if (conn.State == ConnectionState.Closed)
+            {
+                MessageBox.Show("Сначала подключитесь к базе");
+                return;
+            }
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = conn;
+            command.CommandText = "SELECT COUNT(*) FROM Products";
+            int number = (int)command.ExecuteScalar();
+            label1.Text = number.ToString();
+
+        }
+
+    }
+}
