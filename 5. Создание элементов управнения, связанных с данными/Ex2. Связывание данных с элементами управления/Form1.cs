7d5e1bc091c77c74c7d3ab6013ca08ab00c2a7c9
VadimITMO2022 <116649038+VadimITMO2022@users.noreply.github.com>
2023-02-01 18:51:48 +0300

    Add files via upload

---
+++ 5. Создание элементов управнения, связанных с данными/Ex2. Связывание данных с элементами управления/Form1.cs
@@ -0,0 +1,53 @@
+﻿using DataBindingSimple.NorthwindDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBindingSimple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+        }
+
+        //private BindingSource productsBindingSource;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "northwindDataSet.Products". При необходимости она может быть перемещена или удалена.
+            this.productsTableAdapter.Fill(this.northwindDataSet.Products);
+            productsTableAdapter.Fill(northwindDataSet.Products);
+            productsBindingSource = new BindingSource(northwindDataSet, "Products");
+
+            ProductIDTextBox.DataBindings.Add("Text", productsBindingSource, "ProductID");
+            ProductNameTextBox.DataBindings.Add("Text", productsBindingSource, "ProductName");
+
+        }
+
+        private void PreviousButton_Click(object sender, EventArgs e)
+        {
+            productsBindingSource.MovePrevious();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            productsBindingSource.MoveNext();
+        }
+    }
+}
